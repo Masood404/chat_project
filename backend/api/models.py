c981169a875569b0ce6ebc48740278f11647275e
Masood404 <yaquobimasood@gmail.com>
2024-08-10 20:29:08 +0200

    For the User model. Modifed the first_name field so its not optional, added the friends field, added the sent_requests field which's related name is recieved_requests and added methods to add these many to many fields.

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=150, blank=False, null=False)
+    friends = models.ManyToManyField("self", symmetrical=True)
+    sent_requests = models.ManyToManyField("self", symmetrical=False, related_name="recieved_requests")
+
+    def send_request(self, user):
+        if self == user:
+            raise ValidationError("A user cannot send friend request to themselves.")
+        
+        self.sent_requests.add(user)
+
+    def add_friend(self, user):
+        if self == user:
+            raise ValidationError("A user cannot friend themselves.")
+        
+        self.friends.add(user)
