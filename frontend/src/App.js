151147abcba88c4d17ff78b39608f793dccb6c34
Masood404 <yaquobimasood@gmail.com>
2024-09-09 01:15:24 +0200

    Removed the custom color vivid blue from bs's theme colors, instead just directly ovrrided bs's primary color to be vivid-blue.
    Modified the links style to be customly generic instead of explicitly styling them seperately for the modified changed.
    
    For the app element just used bootstrap's utility for some style properties.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,16 +7,10 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App" style={{
-      display: 'flex',
-      flexDirection: 'column',
-      position: 'fixed',
-      height: '100%',
-      width: '100%',
+    <div className="App d-flex flex-column h-100 w-100" style={{
+      position: 'fixed'
     }}>
-      <Container className="py-2 my-2" style={{
-        height: '100%'
-      }}>
+      <Container className="py-2 my-2 h-100">
         <Routes>
           <Route path="/login" element={<Login />} />
         </Routes>
