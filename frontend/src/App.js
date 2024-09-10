a7e5514854dba39b16a098dc395a93ae7ae30a96
Masood404 <yaquobimasood@gmail.com>
2024-09-10 20:37:00 +0200

    Reverted the Routes to be marked up instead of hooked.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,22 @@
-import { Routes, Route, useRoutes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
 import Login from './pages/Login';
-import Home from './pages/Home';
 
 import './App.scss';
-import PageNotFound from './pages/PageNotFound';
 
 function App() {
-    const routes = useRoutes([
-        ...['/', '/home'].map(path => ({ path, element: <Home /> })),
-		{ path: "/login", element: <Login /> },
-		{ path: "*", element: <PageNotFound /> }
-    ]);
-
-    return (
-        <div className="App d-flex flex-column h-100 w-100">
-            <Container className="py-2 my-2 h-100">
-                {routes}
-            </Container>
-        </div>
-    );
+  return (
+    <div className="App d-flex flex-column h-100 w-100" style={{
+      position: 'fixed'
+    }}>
+      <Container className="py-2 my-2 h-100">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
