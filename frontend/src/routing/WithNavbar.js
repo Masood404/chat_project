753d6fe44cd9e86bfea4539f1195e099813c9de0
Masood404 <yaquobimasood@gmail.com>
2024-09-14 18:55:55 +0200

    Made WithNavbar more hybrid or flexible to work on other components other than on a route.

--- frontend/src/routing/WithNavbar.js
+++ frontend/src/routing/WithNavbar.js
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
-const WithNavbar = () => {
+const WithNavbar = ({ isRoute = true, children }) => {
     return (
         <>
             <Navbar />
-            <Outlet />
+            <div className="px-4 py-5">
+                {isRoute ? <Outlet /> : children}
+            </div>
         </>
     );
 };
