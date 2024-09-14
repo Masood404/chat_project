ad7caee28c4ffc51cc9b33f1e8d2157602761094
Masood404 <yaquobimasood@gmail.com>
2024-09-14 15:27:55 +0200

    Added the navbar component and and WithNavbar layout routing.

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import { Navbar as BsNavbar, Nav, Container } from "react-bootstrap";
+
+import MessengerLogo from "../images/messenger-logo.svg";
+
+const Navbar = () => {
+    return (
+        <BsNavbar expand="md" className="p-3 sticky-top">
+            <Container fluid={true}>
+                <BsNavbar.Brand>
+                    <Link to="/"><img src={MessengerLogo} alt="Messenger Navbar Brand" /></Link>
+                </BsNavbar.Brand>
+                <BsNavbar.Toggle aria-controls="basic-navbar-nav" />
+                <BsNavbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto mt-md-0 mt-2">
+                        <Nav.Item className="mx-md-3">
+                            <Nav.Link as={Link} to="/features">Features</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item className="mx-md-3">
+                            <Nav.Link as={Link} to="/download-app">Download app</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </BsNavbar.Collapse>
+            </Container>
+        </BsNavbar>
+    );
+};
+
+export default Navbar;
