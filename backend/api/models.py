567dc29c3b863eb4d284a0a5265dc2736dde7c98
BigM20006 <m1.Yaquobi@gmail.com>
2024-09-29 20:28:48 +0200

    Completely removed the friends feature from the User model. The ability to chat would not be via a friend request it would via a "Chat Request". Thus another model Chat should be added instead of this method.

--- backend/api/models.py
+++ backend/api/models.py
@@ -8,17 +8,3 @@ class User(AbstractUser):
         ordering = ("first_name", "last_name")
 
     first_name = models.CharField(max_length=150, blank=False, null=False)
-    friends = models.ManyToManyField("self", symmetrical=True)
-    sent_requests = models.ManyToManyField("self", symmetrical=False, related_name="recieved_requests")
-
-    def send_request(self, user):
-        if self == user:
-            raise ValidationError("A user cannot send friend request to themselves.")
-        
-        self.sent_requests.add(user)
-
-    def add_friend(self, user):
-        if self == user:
-            raise ValidationError("A user cannot friend themselves.")
-        
-        self.friends.add(user)
