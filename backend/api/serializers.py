18c1e5e25171cbb669c57ce00e0c78b74fca3f47
Masood404 <yaquobimasood@gmail.com>
2024-10-11 19:00:27 +0200

    Removed the get_sender method as it only helped get a single line of statement. When instantiating chat requests while looping through the receivers, now it ignores the creation of the instantiation of chat request if the sender is the same as the receiver.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -93,19 +93,11 @@ class ChatRequestSerializer(serializers.ModelSerializer):
 class ChatRequestCreateSerializer(serializers.Serializer):
     chat = serializers.PrimaryKeyRelatedField(queryset=Chat.objects.all(), write_only=True, required=False)
     name = serializers.CharField(required=False, allow_blank=True, write_only=True)
-    sender = UserSerializer(read_only=True)
     receivers = serializers.ListField(
         child=serializers.PrimaryKeyRelatedField(queryset=User.objects.all()),
         required=True,
         write_only=True
-    )
-
-    def get_sender(self, obj=None):
-        request = self.context['request']
-
-        if not request: return None
-
-        return request.user
+    )   
 
     def validate_chat(self, chat):
         request = self.context.get('request', None)
@@ -119,7 +111,7 @@ class ChatRequestCreateSerializer(serializers.Serializer):
         chat = validated_data.get('chat', None)
         chat_name = validated_data.get('name', None)
 
-        sender = self.get_sender()
+        sender = self.context['request'].user
         receivers = validated_data['receivers']
 
         # If chat id is not provided, then create one
@@ -127,6 +119,10 @@ class ChatRequestCreateSerializer(serializers.Serializer):
             chat = Chat.objects.create(name=chat_name, admin=sender)
         
         # Loop through the receivers and instantiate(not create) ChatRequest(s)
-        chat_requests = [ChatRequest(chat=chat, sender=sender, receiver=receiver) for receiver in receivers]
+        chat_requests = [
+            ChatRequest(chat=chat, sender=sender, receiver=receiver) 
+            for receiver in receivers 
+            if sender != receiver # Ignore this receiver if it is as same as the sender
+        ]
 
         return ChatRequest.objects.bulk_create(chat_requests)
