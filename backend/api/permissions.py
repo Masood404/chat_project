4397794784686a252adc724cfc0f19ceaecccb18
Masood404 <yaquobimasood@gmail.com>
2024-10-12 16:45:33 +0200

    Added IsOwner permission class used to give object based. It can also take owner_fields as an argument on instantiation to specify which field of an object is the owner's id in.

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,7 +1,30 @@
+from typing import Iterable, Union
 from rest_framework.permissions import *
+from django.http import HttpRequest
 
 class ReadIsAuthenticated(BasePermission):
     def has_permission(self, request, view):
         return request.method == "POST" or (
             request.method == "GET" and request.user.is_authenticated
         )
+
+class IsOwner(BasePermission):
+    """
+    Custom permission class to only allow the authorized user or the owner
+    (based on the owner_fields provided as an argument)
+    to do RUD operations on any object.
+    """
+    owner_fields = ['owner']
+
+    def __init__(self, owner_fields: Iterable[str] | None = None):
+        if owner_fields:
+            self.owner_fields = owner_fields
+        super().__init__()
+
+    def has_object_permission(self, request: HttpRequest, view, obj):
+        # Check ownership based on the provided owner_fields
+        for owner_field in self.owner_fields:
+            owner = getattr(obj, owner_field, None)
+            if owner == request.user:
+                return True
+        return False
