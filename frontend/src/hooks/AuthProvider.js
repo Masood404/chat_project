c74be61ce24d85b132cce0d568ff89fbd61ac9b2
Masood404 <yaquobimasood@gmail.com>
2024-10-13 17:00:42 +0200

    Modified the AuthProvider to have a way of getting current logged in user data and other optimizations
    - Created user state that contains data on the current logged in user, it would be null on unauthorized.
    - Utilizing the newly created custom error classes.
    - Added comments on non-self-explainable code.

--- frontend/src/hooks/AuthProvider.js
+++ frontend/src/hooks/AuthProvider.js
@@ -1,3 +1,5 @@
+import { NotFound, UnexpectedResponseData } from "../errors";
+
 import { createContext, useContext, useEffect, useState, useMemo, useLayoutEffect } from "react";
 import axiosInstance from "../axiosInstance";
 
@@ -8,21 +10,28 @@ const AuthProvider = ({ children }) => {
     const refreshToken = localStorage.getItem('refreshToken');
 
     const [accessToken, setAccessToken] = useState(null);
+    const [user, setUser] = useState(null);
 
+    // The state either if the first authentication check is over.
     const [isLoaded, setIsLoaded] = useState(false);
-
+    
     const login = async (formData) => {
-        const { data } = await axiosInstance({
-            method: 'POST',
-            url: '/token/',
-            data: formData
-        });
-
-        if (!data.access || !data.refresh) throw data;
-
-        localStorage.setItem('refreshToken', data.refresh);
-
-        setAccessToken(data.access);
+        try {
+            const { data } = await axiosInstance({
+                method: 'POST',
+                url: '/token/',
+                data: formData
+            });
+            
+            if (!data.access || !data.refresh) throw new UnexpectedResponseData(data);
+    
+            localStorage.setItem('refreshToken', data.refresh);
+            
+            setAccessToken(data.access);
+        } catch (error) {
+            if (error instanceof UnexpectedResponseData) console.error(error.message);
+            else throw error;
+        }
     };
 
     const logout = () => {
@@ -34,7 +43,7 @@ const AuthProvider = ({ children }) => {
     const refreshAccessToken = async () => {
 
         try {
-            if (!refreshToken) throw 'Refresh token not found.';
+            if (!refreshToken) throw new NotFound('Refresh token not found.');
 
             const { data } = await axiosInstance({
                 method: 'POST',
@@ -42,15 +51,38 @@ const AuthProvider = ({ children }) => {
                 data: { refresh: refreshToken }
             });
 
-            if (!data.access) throw data;
+            if (!data.access) throw new UnexpectedResponseData(data);
 
             setAccessToken(data.access);
 
         } catch (error) {
+            if (error instanceof UnexpectedResponseData) console.error(error.message);
+            // Just warn if the refresh token is not found
+            else if (error instanceof NotFound) console.warn(error.message);
+            else throw error;
             logout();
-            console.error(error);
         }
 
+    };
+
+    const loadUser = async () => {
+        try {
+            if (!accessToken) throw new NotFound('Access token not found.');
+
+            const { data } = await axiosInstance('/user/');
+
+            if (!data.id) throw new UnexpectedResponseData(data);
+
+            setUser(data);
+
+        } catch (error) {
+            if (error instanceof UnexpectedResponseData || error instanceof NotFound) {
+                console.error(error.message);
+            }
+            else {
+                throw error;
+            }
+        }
     };
 
     const register = async (formData) => {
@@ -63,19 +95,29 @@ const AuthProvider = ({ children }) => {
 
     useEffect(() => {
         refreshAccessToken()
-            .finally(() => { setIsLoaded(true); });
+            .finally(() => { 
+                // Authentication is check over
+                setIsLoaded(true); 
+            });
     }, []);
 
+    useEffect(() => {
+        if(accessToken) loadUser();
+    }, [accessToken]);
+
+    // Synchronous code to set axios interceptors correctly
     useLayoutEffect(() => {
+        // If Authorized
         if (accessToken) {
             const requestInterceptor = axiosInstance.interceptors.request.use(
                 config => {
+                    // Set the Authorization header for the request interceptor
                     config.headers.Authorization = `Bearer ${accessToken}`;
                     return config;
                 },
                 error => Promise.reject(error)
             );
-
+            
             return () => {
                 axiosInstance.interceptors.request.eject(requestInterceptor);
             };
@@ -84,13 +126,14 @@ const AuthProvider = ({ children }) => {
     }, [accessToken]);
 
     const contextValue = useMemo(() => ({
+        user,
         isLoaded,
         accessToken,
         refreshToken,
         login,
         logout,
         register
-    }), [isLoaded, accessToken, refreshToken]);
+    }), [user, isLoaded, accessToken, refreshToken]);
 
     return (
         <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
