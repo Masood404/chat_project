7245ca09d05acd06c6de66cdeaec0be6000738d2
Masood404 <yaquobimasood@gmail.com>
2024-11-01 22:05:16 +0100

    As of previous commit, the generateUsersString function have been moved to a new file called utils.js

--- frontend/src/components/side-lists/Chats.js
+++ frontend/src/components/side-lists/Chats.js
@@ -25,7 +25,7 @@ const Chats = ({ selfId, chats, currentChatIndex, handleChatChange, handleCompos
                     {chats.map((chat, i) => (
                         <ChatSideButton 
                             key={i}
-                            name={chat.name ?? generateUsersString(chat.users, selfId)}
+                            name={chat.name}
                             summary="Dummy Message for now"
                             onClick={handleChatChange(i)} 
                             active={i === currentChatIndex}
@@ -37,30 +37,3 @@ const Chats = ({ selfId, chats, currentChatIndex, handleChatChange, handleCompos
 }
 
 export default Chats;
-
-function generateUsersString(users, selfId) {
-    if (selfId) {
-        // Filter out the other users from the users array
-        const otherUsers = users.filter(user => user.id !== selfId);
-
-        // If there is only one other user then only return their username
-        if (otherUsers.length === 1) return otherUsers[0].username;
-
-        else if (otherUsers.length > 1) {
-            // Get all the other usernames
-            const usernames = otherUsers.map(user => user.username);
-            // The first three users will be displayed as username1, username2, username3
-            const displayedUsernames = usernames.slice(0, 3).join(', ');
-            // The extra count number if there are more tha three other users
-            const extraCount = otherUsers.length - 3;
-            
-            return extraCount > 0
-                // For example, "username1, username2, username3, 2+" would be displayed if there are exacly 5 users
-                ? `${displayedUsernames}, ${extraCount}+`
-                // For example, "username1, username2, username3" would be displayed if there are exacly 3 users
-                : displayedUsernames;
-        }
-    }
-
-    return 'Empty Chat';
-};
