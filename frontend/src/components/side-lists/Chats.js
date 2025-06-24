1b5d82da9b333db564f515ccfc3b93fd200fd846
Masood404 <yaquobimasood@gmail.com>
2025-06-24 22:08:25 +0200

    Used the Search Input component for the Chats section.

--- frontend/src/components/side-lists/Chats.js
+++ frontend/src/components/side-lists/Chats.js
@@ -1,23 +1,13 @@
-import { useEffect } from "react";
-
 import { ChatSideButton } from "../ChatSideButton";
 import Input from "../Input";
+import Search from "../Search";
 
 const Chats = ({ chats, currentChatIndex, handleChatChange }) => {
     return (
         <div className="h-100 d-flex flex-column pb-3">
             <div className="px-2 py-2 d-flex flex-column">
                 <h1 className="fs-4 fw-bold m-2">Chats</h1>
-                
-                <div className="d-flex align-items-center bg-body-secondary px-3 mt-2 rounded-5">
-                    <i className="bi bi-search d-block"></i>
-                    <Input 
-                        placeholder="Search chats"
-                        containerClass="flex-grow-1"
-                        className="w-100"
-                        customVariant="none"
-                    />
-                </div>
+                <Search placeholder="Search Chats" />
             </div>
             <div className="flex-grow-1 scroll-container-wrapper">
                 <div className="scroll-container px-2">
